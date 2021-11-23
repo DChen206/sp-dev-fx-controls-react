e6e143ea4775195838e2562becedb363db3db61c
Donald Chen <donald.chen@alaskaair.com>
2021-11-23 12:16:08 -0800

    Commit Built Files

---
+++ lib/webparts/controlsTest/components/IControlsTestProps.d.ts
@@ -0,0 +1,45 @@
+/// <reference types="react" />
+import { WebPartContext } from '@microsoft/sp-webpart-base';
+import { ImageSize } from '../../../FileTypeIcon';
+import { DisplayMode } from '@microsoft/sp-core-library';
+import { IProgressAction } from '../../../Progress';
+import { IFilePickerResult } from '../../../FilePicker';
+import { ITag } from 'office-ui-fabric-react';
+import { IReadonlyTheme } from "@microsoft/sp-component-base";
+export interface IControlsTestProps {
+    context: WebPartContext;
+    description: string;
+    title: string;
+    displayMode: DisplayMode;
+    updateProperty: (value: string) => void;
+    totalPages?: number;
+    themeVariant?: IReadonlyTheme;
+}
+export interface IControlsTestState {
+    imgSize: ImageSize;
+    items: any[];
+    initialValues: any[];
+    iFrameDialogOpened?: boolean;
+    iFramePanelOpened?: boolean;
+    authorEmails: string[];
+    selectedList: string;
+    progressActions: IProgressAction[];
+    currentProgressActionIndex?: number;
+    dateTimeValue: Date;
+    richTextValue: string;
+    currentCarouselElement: JSX.Element;
+    canMovePrev: boolean;
+    canMoveNext: boolean;
+    comboBoxListItemPickerListId: string;
+    comboBoxListItemPickerIds: any[];
+    filePickerResult?: IFilePickerResult[];
+    treeViewSelectedKeys?: string[];
+    showAnimatedDialog?: boolean;
+    showCustomisedAnimatedDialog?: boolean;
+    showSuccessDialog?: boolean;
+    showErrorDialog?: boolean;
+    selectedTeam: ITag[];
+    selectedTeamChannels: ITag[];
+    filePickerDefaultFolderAbsolutePath?: string;
+}
+//# sourceMappingURL=IControlsTestProps.d.ts.map
---
+++ lib/webparts/controlsTest/components/TestControl.d.ts
@@ -0,0 +1,7 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+import * as React from "react";
+export interface ITestControlProps {
+    context: WebPartContext;
+}
+export declare const TestControl: React.FunctionComponent<ITestControlProps>;
+//# sourceMappingURL=TestControl.d.ts.map
